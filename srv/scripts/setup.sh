f2ebe0a32bff925772a5aad2886fd89b20f69e3e
Hakira-Shymuy <hakira.shymuy@gmail.com>
2025-02-27 00:59:06 +0100

    skip: try to fix srv directory problem

--- srv/scripts/setup.sh
+++ srv/scripts/setup.sh
@@ -20,9 +20,9 @@ fi
 echo "Cloning the vps-setup repository..."
 sudo git clone https://github.com/Hakira-Shymuy/vps-setup.git "$TEMP_DIR"
 
-# Change to the srv directory
-echo "Changing directory to /srv..."
-sudo mv "$TEMP_DIR/srv" /srv/
+# Move only the CONTENTS of the "srv" folder into /srv/
+echo "Changing directory files to /srv/..."
+sudo mv "$TEMP_DIR/srv/"* /srv/
 
 # Clean up
 echo "Cleaning up..."
@@ -31,6 +31,4 @@ sudo rm -rf "$TEMP_DIR"
 echo "Running server setup..."
 # sudo bash /srv/scripts/setup-server.sh
 
-echo "VPS Setup Complete!"
-
-echo "Server setup complete!"
+echo "Server Setup Complete!"
